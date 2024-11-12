47a0da677090fdbd96186b6f51fb9764b17cb06e
Felix Otieno <omondifelixotieno2@gmail.com>
2024-11-12 08:46:05 +0300

    fundamentals on function

---
+++ Functions/default.js
@@ -0,0 +1,7 @@
+function hello(name = "Chris") {
+    console.log(`Hello ${name}!`);
+  }
+  
+  hello("Ari"); // Hello Ari!
+  hello(); // Hello Chris!
+  
---
+++ Functions/defaultValue.js
@@ -0,0 +1,5 @@
+function showMessage(from, text = "no text given") {
+    console.log( from + ": " + text );
+  }
+  
+  showMessage("Ann"); // Ann: no text given
